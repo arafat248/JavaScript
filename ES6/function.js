97edc2e0abb7c5fff461056b68cafdb4487be4d8
arafat248 <aab.arafat248@gmail.com>
2026-05-11 21:46:57 +0600

    practice find, filter, map

--- ES6/function.js
+++ ES6/function.js
@@ -43,9 +43,53 @@
 // console.log(name, age, discription);
 
 
-function arrow(num1, num2){
-    const result=num1+num2;
-    return result;
-}
-const output=arrow(20, 30);
-console.log(output);
+// function arrow(num1, num2){
+//     const result=num1+num2;
+//     return result;
+// }
+// const output=arrow(20, 30);
+// console.log(output);
+
+const products = [
+        {
+            id: 1,
+            name: "Smartphone",
+            brand: "Samsung",
+            color: "Black",
+            model: "Galaxy S23"
+        },
+        {
+            id: 2,
+            name: "Laptop",
+            brand: "Dell",
+            color: "Silver",
+            model: "XPS 13"
+        },
+        {
+            id: 3,
+            name: "Headphones",
+            brand: "Sony",
+            color: "Black",
+            model: "WH-1000XM5"
+        },
+        {
+            id: 4,
+            name: "Smartwatch",
+            brand: "Apple",
+            color: "Space Gray",
+            model: "Series 9"
+        },
+        {
+            id: 5,
+            name: "Tablet",
+            brand: "Lenovo",
+            color: "Blue",
+            model: "Tab P11"
+        }
+        ];
+const result=products.find(pd=>pd.id==3);
+console.log(result);
+const results=products.filter(pd=>pd.color=="Black");
+console.log(...results);
+const map=products.map(pd=>pd.id*5);
+console.log(map);
