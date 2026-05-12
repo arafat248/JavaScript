43c75a78d1db81dc09d3bf759dfbb285199f2a27
arafat248 <aab.arafat248@gmail.com>
2026-05-12 22:53:32 +0600

    Display All Products

--- Project/project.js
+++ Project/project.js
@@ -1,1 +1,22 @@
-alert()
+const loadallProducts = ()=>{
+    fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(data => {
+        displayProducts(data);
+;    })
+}
+const displayProducts = product =>{
+    const productcontainer = document.getElementById("product-container");
+    product.forEach(element => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <img src="${element.image}" alt="${element.title}">
+        <h4>${element.title}</h4>
+        <p>${element.description}</p>
+        <button>Details</button>
+        <button> Add to Cart </button>
+        `;
+        productcontainer.appendChild(div);
+    });
+}
+loadallProducts();
